f7e032f207fbe3d4744ba286e9eefae09c4834bc
cyroz <88747370+cyroz1@users.noreply.github.com>
2022-11-18 16:32:43 -0500

    Add files via upload

--- launch.cmd
+++ launch.cmd
@@ -9,5 +9,15 @@ GOTO :search
 :found
 taskkill /F /IM EpicGamesLauncher.exe
 taskkill /F /IM EpicWebHelper.exe
+GOTO :search2
+
+:search2
+tasklist|find "CrashReportClient.exe"
+IF %ERRORLEVEL% == 0 GOTO :found2
+TIMEOUT /T 1
+GOTO :search2
+
+:found2
 taskkill /F /IM CrashReportClient.exe
+
 exit
